b900b6bd81c26e2316e5ac3df711d835e992c267
Kenny Nguyen <kenny.nguyen@fdmgroup.com>
2023-02-20 14:56:40 -0500

    Implement node and actions components

--- src/app/components/actions/actions.component.ts
+++ src/app/components/actions/actions.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { Action } from '../../models/action';
 
 @Component({
-  selector: 'app-actions',
-  templateUrl: './actions.component.html',
-  styleUrls: ['./actions.component.scss']
+    selector: 'app-actions',
+    templateUrl: './actions.component.html',
+    styleUrls: ['./actions.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ActionsComponent {
+export class ActionsComponent implements OnInit {
+
+    @Input('actions') actions: Array<any> = [];
+    @Output('taken') taken: EventEmitter<Action> = new EventEmitter<Action>();
+
+    constructor() { }
+
+    ngOnInit() { }
+
+    actionClick(action) {
+        this.taken.emit(action);
+    }
+
+    drop(event: Event) {
+        event.preventDefault();
+    }
 
 }
--- src/app/components/node/node.component.ts
+++ src/app/components/node/node.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, ChangeDetectionStrategy, ChangeDetectorRef, ViewChild, ElementRef, SimpleChanges } from '@angular/core';
+import { Node } from '../../models/node';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
-  selector: 'app-node',
-  templateUrl: './node.component.html',
-  styleUrls: ['./node.component.scss']
+    selector: 'app-node',
+    templateUrl: './node.component.html',
+    styleUrls: ['./node.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NodeComponent {
+export class NodeComponent implements OnInit, OnChanges {
+
+    @Input('node') node: Node;
+    @Input('isClicked') isClicked: boolean;
+    @Output('dropped') dropped: EventEmitter<any> = new EventEmitter<any>();
+    @ViewChild('nodeElement', { static: true }) nodeElement;
+
+    isStartNode = false;
+    isEndNode = false;
+
+    constructor(private messageService: MessageService, private ref: ChangeDetectorRef, private elementRef: ElementRef) { }
+
+    ngOnInit(): void {
+        this.messageService.messages$.subscribe(
+            (message: Node) => {
+                console.log('Message:', message);
+            }
+        );
+    }
+
+    ngAfterViewInit() { }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('Changes:', changes);
+    }
+
+    runChangeDetector() {
+        this.ref.markForCheck();
+    }
+
+    mouseUp(event: Event) {
+        try {
+            var data = (event as any).dataTransfer.getData('text');
+            this.dropped.emit({
+                previousNode: JSON.parse(data),
+                newNode: this.node
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    mouseDown(event: Event) {
+        if (this.node.isStartNode || this.node.isEndNode) {
+            this.messageService.setMouseReleased();
+            event.stopPropagation();
+            return;
+        }
+        this.node.isWall = !this.node.isWall;
+    }
+
+    createWall(event) {
+        if (this.messageService.getMouseClicked() == true && !this.node.isStartNode && !this.node.isEndNode) {
+            this.node.isWall = !this.node.isWall;
+        }
+    }
+
+    dragCancel(event: Event) {
+        event.preventDefault();
+    }
+
+    dragStart(event) {
+        event.dataTransfer.setData('text/plain', JSON.stringify(this.node));
+        event.data = this.node;
+    }
 
 }
